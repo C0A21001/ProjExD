52f3bb35824072d26586e3b5cc20f43ffeb2058a
kohei.A <c0a210015c@edu.teu.ac.jp>
2022-11-29 15:11:51 +0900

    和電卓

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,7 +4,16 @@ import tkinter.messagebox as tkm
 def button_click(event):
     btn = event.widget
     txt = btn["text"]
-    tkm.showinfo(txt,f"{txt}のボタンがクリックされました")
+    #tkm.showinfo(txt,f"{txt}のボタンがクリックされました")
+    if txt == "+":
+        entry.insert(tk.END,"+")
+    elif txt == "=":
+        ans = eval(entry.get())
+        entry.delete(0,tk.END)
+        entry.insert(tk.END,ans)
+
+    else:
+        entry.insert(tk.END,f"{txt}")
 
 root = tk.Tk()
 root.title("")
@@ -22,4 +31,10 @@ for i in range(9,-1,-1):
     if c%3 == 0:
         r+=1
         c=0
+button = tk.Button(root,text="+",font=("",30),width=4,height=2)
+button.grid(row=4,column=1)
+button.bind("<1>", button_click)
+button = tk.Button(root,text="=",font=("",30),width=4,height=2)
+button.grid(row=4,column=2)
+button.bind("<1>", button_click)
 root.mainloop()
