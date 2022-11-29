0ae1db445517d61a3403076d7eec3d23a88de345
kohei.A <c0a210015c@edu.teu.ac.jp>
2022-11-29 16:43:54 +0900

    電卓

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,9 +2,10 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 def button_click(event):
+    if entry.get() == "0":
+        entry.delete(0,tk.END)
     btn = event.widget
     txt = btn["text"]
-    #tkm.showinfo(txt,f"{txt}のボタンがクリックされました")
     if txt == "+":
         entry.insert(tk.END,"+")
     elif txt == "=":
@@ -13,14 +14,31 @@ def button_click(event):
         entry.insert(tk.END,ans)
     elif txt == "-":
         entry.insert(tk.END,"-")
+    elif txt == "×":
+        entry.insert(tk.END,"*")
+    elif txt == "÷":
+        entry.insert(tk.END,"/")
+    elif txt == "c":
+        entry.delete(0,tk.END)
+        entry.insert(tk.END,"0")
+    elif txt == "x²":
+        nom = eval(entry.get())
+        entry.delete(0,tk.END)
+        ans = nom**2
+        entry.insert(tk.END,ans)
+    elif txt == "±":
+        nom = eval(entry.get())
+        entry.delete(0,tk.END)
+        ans = -1*nom
+        entry.insert(tk.END,ans)
     else:
         entry.insert(tk.END,f"{txt}")
 
 root = tk.Tk()
-root.title("")
-root.geometry("300x500")
+root.title("電卓")
+root.geometry("290x500")
 entry = tk.Entry(justify="right",width = 10,font=("",40))
-entry.insert(tk.END,"")
+entry.insert(tk.END,"0")
 entry.grid(columnspan = 4)
 r = 225
 c = 0
@@ -40,5 +58,23 @@ button.place(x=219,y=435)
 button.bind("<1>", button_click)
 button = tk.Button(root,text="-",font=("",30),width=3,height=1)
 button.place(x=219,y=295)
+button.bind("<1>", button_click)
+button = tk.Button(root,text="×",font=("",30),width=3,height=1)
+button.place(x=219,y=225)
+button.bind("<1>", button_click)
+button = tk.Button(root,text="÷",font=("",30),width=3,height=1)
+button.place(x=219,y=155)
+button.bind("<1>", button_click)
+button = tk.Button(root,text="c",font=("",30),width=3,height=1)
+button.place(x=0,y=155)
+button.bind("<1>", button_click)
+button = tk.Button(root,text=".",font=("",30),width=3,height=1)
+button.place(x=73,y=435)
+button.bind("<1>", button_click)
+button = tk.Button(root,text="x²",font=("",30),width=3,height=1)
+button.place(x=73,y=155)
+button.bind("<1>", button_click)
+button = tk.Button(root,text="±",font=("",30),width=3,height=1)
+button.place(x=146,y=435)
 button.bind("<1>", button_click)
 root.mainloop()
