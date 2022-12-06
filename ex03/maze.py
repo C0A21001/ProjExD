17348c807a83f8b17de969df62182ee3a441780b
kohei.A <c0a210015c@edu.teu.ac.jp>
2022-12-06 17:40:44 +0900

    提出用

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+import maze_maker as mm
 
 def key_down(event):
     global key
@@ -10,17 +11,33 @@ def key_up(event):
     key = " "
 
 def main_proc():
-    global cx,cy
-    if key == "Up":
-        cy -= 20
-    elif key == "Down":
-        cy += 20
-    elif key == "Left":
-        cx -=20
-    elif key == "Right":
-        cx += 20
+    global mx,my#移動キーを変更
+    if key == "w":
+        my -= 1
+    elif key == "s":
+        my += 1
+    elif key == "a":
+        mx -= 1
+    elif key == "d":
+        mx += 1
+    if maze_lst[mx][my] == 1:
+        if key == "w":
+            my += 1
+        elif key == "s":
+            my -= 1
+        elif key == "a":
+            mx += 1
+        elif key == "d":
+            mx -= 1
+    cx,cy = mx*100+50,my*100+50
     canvas.coords("tori",cx,cy)
-    root.after(10, main_proc)
+    if mx == 13 and my == 7:
+        tkm.showinfo("クリア","ゴール！")#ゴール時に表示
+        mx = 1
+        my = 1
+        cx,cy = mx*100+50,my*100+50
+        canvas.coords("tori",cx,cy)
+    root.after(100, main_proc)
 
 
 if __name__ == "__main__":
@@ -28,11 +45,16 @@ if __name__ == "__main__":
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(width = "1500",height = "900",bg = "black")
     canvas.pack()
+    maze_lst = mm.make_maze(15,9)
+    #print(maze_lst)
+    mm.show_maze(canvas,maze_lst)
     key = " "
-    cx,cy = 300,400
+    mx,my = 1,1
+    cx,cy = mx*100+50,my*100+50
     image = tk.PhotoImage(file = "fig/8.png")
     canvas.create_image(cx,cy,image = image,tag = "tori")
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
+
     main_proc()
     root.mainloop()
