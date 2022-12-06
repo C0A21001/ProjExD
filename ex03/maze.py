7f13e2449f819fdfe24996e510d006615399082e
kohei.A <c0a210015c@edu.teu.ac.jp>
2022-12-06 14:30:13 +0900

    key初期化

---
+++ ex03/maze.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん")
+    canvas = tk.Canvas(width = "1500",height = "900",bg = "black")
+    canvas.pack()
+    key = ""
+    cx,cy = 300,400
+    image = tk.PhotoImage(file = "fig/8.png")
+    canvas.create_image(cx,cy,image = image)
+    root.mainloop()
