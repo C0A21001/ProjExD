0fc5246861901d81e30265c3a4a5617d7a6cc189
kohei.A <c0a210015c@edu.teu.ac.jp>
2022-12-06 17:49:32 +0900

    編集

--- ex03/maze.py
+++ ex03/maze.py
@@ -46,7 +46,6 @@ if __name__ == "__main__":
     canvas = tk.Canvas(width = "1500",height = "900",bg = "black")
     canvas.pack()
     maze_lst = mm.make_maze(15,9)
-    #print(maze_lst)
     mm.show_maze(canvas,maze_lst)
     key = " "
     mx,my = 1,1
